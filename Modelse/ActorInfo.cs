8b4990201a715953f0f8b4616d7d6b2a061dc94f
Kurt Hayes <khayes39@uncc.edu>
2024-02-27 16:13:03 -0500

    Add project files.

---
+++ Modelse/ActorInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mySQL_database_code.Modelse;
+
+public partial class ActorInfo
+{
+    public ushort ActorId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? FilmInfo { get; set; }
+}
---
+++ Modelse/SalesByStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mySQL_database_code.Modelse;
+
+public partial class SalesByStore
+{
+    public string? Store { get; set; }
+
+    public string? Manager { get; set; }
+
+    public decimal? TotalSales { get; set; }
+}
